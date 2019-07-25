88f9cd8f80b4457a8e0c0503b141d1854c084f78
sudchaya.somniyarm <sudchaya.somniyarm@scb.co.th>
2019-07-25 15:01:15 +0700

    no message

--- liff-starter.js
+++ liff-starter.js
@@ -6,12 +6,19 @@ window.onload = function (e) {
 
 function initializeApp(data) {
     
-
-        liff.openWindow({
+  // openSCB call
+  document.getElementById('OKbutton').addEventListener('click', function () {
+    liff.openWindow({
             url:'scbeasysim://billpayment-anonymous/a197dc4a-df4e-4c10-ba73-63a619e18066',
             external:true
-          });
-  
+    });
+});
+
+    // closeWindow call
+    document.getElementById('Cancelbutton').addEventListener('click', function () {
+        liff.closeWindow();
+    });
+
 
 }
 
